ab4501a5a025332b14ecbdac9fd2c324ed99349e
jjhbotache <jjhuertasbotache@gmail.com>
2023-01-20 22:00:37 -0500

    ejer cap 4 r3 ended

---
+++ Ejercicios_Python_Juan_Jose_Huertas_Botache/Capitulo 4/ejer_cap4_r2.py
@@ -0,0 +1,8 @@
+""" 
+Escriba un programa en Python que acepte una cadena de texto e indique si todos sus
+caracteres son alfabéticos (solución).
+Entrada: hello-world
+Salida: Se han encontrado caracteres no alfabéticos
+""" 
+if input("Introduce your entry to verify if it's Alphanumeric \n: ").isalpha():print("It is Alphanumeric")
+else:print("It isn't Alphanumeric")
